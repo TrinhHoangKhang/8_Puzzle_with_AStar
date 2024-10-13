221c269a0ba9b1a54281a84b2795a493aaf7b5b2
TrinhHoangKhang <trinhhoangkhangvl@gmail.com>
2024-10-13 22:20:08 +0700

    Full game finished

--- Puzzle_Solver.py
+++ Puzzle_Solver.py
@@ -1,20 +1,20 @@
 import numpy as np
 import heapq
 import copy
+import time
+import logging
 
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(message)s')
 def swap_element(matrix, pos1, pos2):
-    rows, cols = matrix.shape
-    if not (0 <= pos2[0] < rows and 0 <= pos2[1] < cols):
-        raise ValueError("Invalid move: position out of bounds")
-
-    matrix = copy.copy(matrix)
     matrix[pos1], matrix[pos2] = matrix[pos2], matrix[pos1]
-    return matrix
+    return None
 
 class State:
-    def __init__(self, matrix: np.ndarray, blank_pos: tuple) -> None:
+    def __init__(self, matrix: np.ndarray, blank_pos: tuple, g: int) -> None:
         self.matrix = matrix
         self.blank_pos = blank_pos
+        self.g = g
 
     def __eq__(self, value: object) -> bool:
         if not isinstance(value, State):
@@ -22,7 +22,10 @@ class State:
         return np.array_equal(self.matrix, value.matrix) and (self.blank_pos == value.blank_pos)
 
     def __lt__(self, other: object):
-        return self.heuristic() < other.heuristic()
+        return (self.heuristic() + self.g) < (other.heuristic() + other.g)
+
+    def __hash__(self):
+        return hash(str(self.matrix))
 
     def heuristic(self):
         total_distance = 0
@@ -35,47 +38,70 @@ class State:
                     total_distance += abs(row - true_row) + abs(col - true_col)
         return total_distance
 
+    def solvable(self):
+        # Flatten the matrix
+        # Count the each pair, if a pair is inverse then +1
+        # If the result is even: solvable
+        flat = self.matrix.flatten()
+        n = len(flat)
+        count = 0
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                if flat[i] > flat[j]:
+                    count += 1
+
+        if (count % 2) == 0:
+            return True
+        else:
+            return False
+
 class Puzzle_Solver:
     def __init__(self, rows=3, cols=3) -> None:
         self.rows = rows
         self.cols = cols
         # Start state is random, 'space' is at the lower right corner
-        # flat_nums = np.arange(1, 9)
-        # np.random.shuffle(flat_nums)
-        # flat_nums = np.append(flat_nums, 0)
-        flat_nums = np.array([1, 2, 3, 4, 5, 6, 7, 0, 8])
-        start_matrix = flat_nums.reshape((3, 3))
-        self.start = State(start_matrix, (2, 1))
+        # The state must be solvable
+        self.randomize_start_state()
         # End state is ordered numbers, 'space' is at the lower right corner
         flat_nums = np.arange(1, 9)
         flat_nums = np.append(flat_nums, 0)
         end_matrix = flat_nums.reshape((3, 3))
-        self.end = State(end_matrix, (2, 2))
+        self.end = State(end_matrix, (2, 2), 0)
 
         # Flag
         self.solution_exist = False
 
+    def randomize_start_state(self):
+        while True:
+            flat_nums = np.arange(1, 9)
+            np.random.shuffle(flat_nums)
+            flat_nums = np.append(flat_nums, 0)
+            start_matrix = flat_nums.reshape((3, 3))
+            self.start = State(start_matrix, (2, 2), 0)
+
+            if self.start.solvable():
+                logging.info("Solvable state found!")
+                break
+            logging.info("State not solvable, trying a new starting state...")
+
     # Base on current state, return valid neighbors state
     def get_valid_neighbors(self, curr_state: State, closed_set: set):
-        curr_matrix = copy.deepcopy(curr_state.matrix)
-
         curr_row, curr_col = curr_state.blank_pos
 
         next_poss = [(curr_row + 1, curr_col), (curr_row - 1, curr_col),
                      (curr_row, curr_col + 1), (curr_row, curr_col - 1)]
 
         results = []
-        for pos in next_poss:
-            try:
-                next_matrix = swap_element(
-                    curr_matrix, curr_state.blank_pos, pos)
-
-                if tuple(next_matrix.flatten()) in closed_set:
-                    continue
-
-                results.append(State(next_matrix, pos))
-            except:
-                continue
+        for new_pos in next_poss:
+            if (
+                0 <= new_pos[0] < self.rows and
+                0 <= new_pos[1] < self.cols
+            ):
+                new_matrix = copy.deepcopy(curr_state.matrix)
+                swap_element(new_matrix, curr_state.blank_pos, new_pos)
+                new_state = State(new_matrix, new_pos, curr_state.g + 1)
+                if new_state not in closed_set:
+                    results.append(new_state)
 
         return results
 
@@ -84,7 +110,7 @@ class Puzzle_Solver:
         curr = self.end
 
         while curr != self.start:
-            curr = parrent[tuple(curr.matrix.flatten())]
+            curr = parrent[curr]
             path.append(curr.blank_pos)
 
         return path[::-1]
@@ -94,18 +120,21 @@ class Puzzle_Solver:
         closed_set = set()
         parrent = {}
 
+        # Push the first element to the queue
         heapq.heappush(
             priority_queue, self.start)
 
         while priority_queue:
             # Pop the top
             top_state = heapq.heappop(priority_queue)
+            logging.info(f"State explored: {len(closed_set)}")
+            logging.info(f'g: {top_state.g}, h: {top_state.heuristic()}')
             # Check if it is end state
             if top_state == self.end:
                 self.solution_exist = True
                 break
-            # Push the flatten of the matrix to set
-            closed_set.add(tuple(top_state.matrix.flatten()))
+            # Push the state to the closed set
+            closed_set.add(top_state)
 
             # Get the neighbors
             neighbors = self.get_valid_neighbors(top_state, closed_set)
@@ -115,15 +144,10 @@ class Puzzle_Solver:
                     heapq.heappush(
                         priority_queue, neighbor)
                     # Update the parrent
-                    parrent[tuple(neighbor.matrix.flatten())] = top_state
+                    parrent[neighbor] = top_state
 
         # Return the path
         if self.solution_exist:
             path = self.get_path(parrent)
             return path
         return None
-
-
-solver = Puzzle_Solver()
-path = solver.perform_search()
-print(path)
--- pygame_visualizer.py
+++ pygame_visualizer.py
@@ -12,10 +12,10 @@ SCREEN_HEIGHT = GAME_SIZE * SQUARE_SIZE + SQUARE_SIZE / 2
 BUTTON_REGION_HEIGHT = SCREEN_HEIGHT - SCREEN_WIDTH
 # Color constant
 BUTTON_REGION_C = (47, 255, 255)
-SQUARE_C = (185, 185, 185)
-# BLANK_SQUARE_C = (255, 146, 50)
+SQUARE_C = (249, 249, 69)
 BLANK_SQUARE_C = (255, 255, 255)
 BUTTON_C = (108, 238, 238)
+FADED_BUTTON_C = (150, 150, 150)
 # Elements
 game_region = pygame.Rect(0, 0, SCREEN_WIDTH, SCREEN_WIDTH)
 button_region = pygame.Rect(
@@ -27,16 +27,17 @@ run_button = pygame.Rect(SCREEN_WIDTH / 2, SCREEN_WIDTH,
 
 # Running state variable
 puzzle_solver = Puzzle_Solver()
-matrix = puzzle_solver.start.matrix
 blank_pos = puzzle_solver.start.blank_pos
 path = []
+is_moving_piece = False
+
 
 def draw_region(screen):
     pygame.draw.rect(screen, 'white', game_region)
     pygame.draw.rect(screen, BUTTON_REGION_C, button_region)
 
 def draw_squares(screen):
-    global matrix, blank_pos
+    global blank_pos
     font = pygame.font.Font(None, 72)
 
     for row in range(GAME_SIZE):
@@ -54,34 +55,39 @@ def draw_squares(screen):
 
             # Draw the number
             if (row, col) != blank_pos:
-                text = font.render(str(matrix[row, col]), True, (0, 0, 9))
+                text = font.render(
+                    str(puzzle_solver.start.matrix[row, col]), True, (0, 0, 9))
                 text_rect = text.get_rect(
                     center=(x + SQUARE_SIZE // 2, y + SQUARE_SIZE // 2))
                 screen.blit(text, text_rect)
 
 def draw_button(screen):
+    button_c = FADED_BUTTON_C if is_moving_piece else BUTTON_C
+
     font = pygame.font.Font(None, 45)
-    pygame.draw.rect(screen, BUTTON_C, reset_button)
+    pygame.draw.rect(screen, button_c, reset_button)
     pygame.draw.rect(screen, (0, 0, 0), reset_button, 2)
     text = font.render('Reset', True, (0, 0, 9))
     text_rect = text.get_rect(center=(reset_button.center))
     screen.blit(text, text_rect)
 
-    pygame.draw.rect(screen, BUTTON_C, run_button)
+    pygame.draw.rect(screen, button_c, run_button)
     pygame.draw.rect(screen, (0, 0, 0), run_button, 2)
     text = font.render('Run', True, (0, 0, 9))
     text_rect = text.get_rect(center=(run_button.center))
     screen.blit(text, text_rect)
 
 def update_state_on_path():
-    global path, matrix, blank_pos
+    global path, blank_pos, is_moving_piece
     for new_pos in path:
-        matrix[blank_pos], matrix[new_pos] = matrix[new_pos], matrix[blank_pos]
+        puzzle_solver.start.matrix[blank_pos], puzzle_solver.start.matrix[
+            new_pos] = puzzle_solver.start.matrix[new_pos], puzzle_solver.start.matrix[blank_pos]
         blank_pos = new_pos
-        time.sleep(2)
+        time.sleep(0.25)
+    is_moving_piece = False
 
 def run_game():
-    global path
+    global path, is_moving_piece
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
@@ -93,12 +99,18 @@ def run_game():
                 running = False
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if run_button.collidepoint(event.pos):
-                    path = puzzle_solver.perform_search()
-                    print(path)
-                    # A theard that constantly update the state base on path list
-                    # t = threading.Thread(target=update_state_on_path)
-                    # t.start()
+                if not is_moving_piece:
+                    if run_button.collidepoint(event.pos):
+                        path = puzzle_solver.perform_search()
+                        print("Path: \n", path)
+                        is_moving_piece = True
+                        # A theard that constantly update the state base on path list
+                        t = threading.Thread(target=update_state_on_path)
+                        t.start()
+
+                    if reset_button.collidepoint(event.pos):
+                        print("\n Generate new random game...")
+                        puzzle_solver.randomize_start_state()
 
         # Do something
         draw_squares(screen)
